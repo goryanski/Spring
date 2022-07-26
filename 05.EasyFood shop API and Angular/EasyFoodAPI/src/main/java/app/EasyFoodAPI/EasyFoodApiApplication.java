ae8685ba291340727ccf97597108455f4c6cd9b7
goryanski <53867408+goryanski@users.noreply.github.com>
2022-07-26 22:40:25 +0300

    fifth app started

---
+++ 05.EasyFood shop API and Angular/EasyFoodAPI/src/main/java/app/EasyFoodAPI/EasyFoodApiApplication.java
@@ -0,0 +1,19 @@
+package app.EasyFoodAPI;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EasyFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFoodApiApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+}
