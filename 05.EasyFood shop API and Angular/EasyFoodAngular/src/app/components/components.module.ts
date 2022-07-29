88cefa861a7e61e411259e9c7394a8f261a351e7
goryanski <53867408+goryanski@users.noreply.github.com>
2022-07-29 22:44:00 +0300

    reload client

---
+++ 05.EasyFood shop API and Angular/EasyFoodAngular/src/app/components/components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from "@angular/router";
+
+
+// common components
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+
+export class ComponentsModule { }
---
+++ 05.EasyFood shop API and Angular/EasyFoodAngular/src/app/shared/app-environment.interface.ts
@@ -0,0 +1,6 @@
+export interface AppEnvironment {
+  apiEasyFoodURL: string,
+  apiStaticFilesURL: string
+}
+
+export class AppEnvironment implements AppEnvironment {}
